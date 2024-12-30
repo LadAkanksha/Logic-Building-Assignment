98c505cc9bac58713cb2db680f569e5b7490b573
LadAkanksha <akankshalad2003@gmail.com>
2024-12-30 22:46:33 +0530

    Assignment

---
+++ Assignment 09/03 Accept number from user and display below pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iNo)
+{
+    int i = 0;
+    for( i = 1 ; i <= iNo ; ++i)
+    {
+        printf("%d  *  ",i);
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("\nEnter Number of Elements : ");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    getch();
+    return 0;
+}
