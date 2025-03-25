9e9190cfa7e3700b25cc738a96f61a40d72d45e3
LadAkanksha <akankshalad2003@gmail.com>
2025-03-25 22:38:46 +0530

    Assignment

---
+++ Assignment 26/03 Write a program which accept string from user and copy that characters of that string into another string by converting all small characters into capital case.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int StrCpyCap(char *src,char *dest)
+{
+    int i = 0 , j = 0;
+
+    while( src[i] != '\0' )
+    {
+        if(src[i] >= 'a' && src[i] <= 'z')
+        {
+            dest[i] = src[i]-32;
+        }
+        else
+        {
+            dest[i] = src[i];
+        }
+        i++;
+    }
+}
+int main()
+{
+    char cArr[30] = "Marvellous Python 2" , cBrr[30]="";
+
+    StrCpyCap(cArr,cBrr);
+
+    printf("\n%s.",cBrr);
+
+    getch();
+    return 0;
+}
